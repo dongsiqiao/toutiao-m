be23b08562f21930125528e0168701162d7b9902
dongsiqiao <1247143911@qq.com>
2021-02-23 18:04:55 +0800

    初始化目录结构

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
-import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 import './styles/index.less'
+
+import Vue from 'vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+import 'amfe-flexible'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
